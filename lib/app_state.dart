8eb32e5569bea369b27ab9a242f7b84e24296d15
qiaochao <qiaochao@icourt.cc>
2023-08-06 23:52:36 +0800

    feat : login and logout

--- lib/app_state.dart
+++ lib/app_state.dart
@@ -1,1 +1,40 @@
+// ignore_for_file: unused_import, null_argument_to_non_null_type
 
+import 'dart:ffi';
+import 'package:firebase_auth/firebase_auth.dart'
+    hide EmailAuthProvider, PhoneAuthProvider;
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:flutter/material.dart';
+import 'firebase_options.dart';
+
+class ApplicationState extends ChangeNotifier {
+  ApplicationState() {
+    init();
+  }
+  bool _loggedIn = false;
+  bool get loggedIn => _loggedIn;
+
+  //因为返回了null，所以这里定义为Void？
+  Future<Void?> init() async {
+    //初始化
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform);
+    //设置功能
+    FirebaseUIAuth.configureProviders([
+      EmailAuthProvider(),
+    ]);
+
+    //初始化监听
+    FirebaseAuth.instance.userChanges().listen((user) {
+      if (user != null) {
+        _loggedIn = true;
+      } else {
+        _loggedIn = false;
+      }
+      notifyListeners();
+    });
+
+    return null;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,123 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+// ignore_for_file: unused_import
 
+import 'package:firebase_ui_auth/firebase_ui_auth.dart'; // new
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart'; // new
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart'; // new
+
+import 'app_state.dart'; // new
 import 'home_page.dart';
 
+//进行身份验证流程
+
 void main() {
-  runApp(const App());
+  //将provider初始化和配置加上
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ApplicationState(),
+      builder: ((context, child) => const App()),
+    ),
+  );
 }
 
+// Add GoRouter configuration outside the App class
+// 放一个全局路由
+//发，GoRouter.of(context).go('/details', queryParams: {'id': '123'});
+//接，final queryParams = GoRouter.of(context).queryParams;
+final _router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const HomePage(),
+      routes: [
+        GoRoute(
+          path: 'sign-in',
+          builder: (context, state) {
+            return SignInScreen(
+              actions: [
+                ForgotPasswordAction(((context, email) {
+                  final uri = Uri(
+                    path: '/sign-in/forgot-password',
+                    queryParameters: <String, String?>{
+                      'email': email,
+                    },
+                  );
+                  context.push(uri.toString());
+                })),
+                AuthStateChangeAction(((context, state) {
+                  final user = switch (state) {
+                    SignedIn state => state.user,
+                    UserCreated state => state.credential.user,
+                    _ => null
+                  };
+                  if (user == null) {
+                    return;
+                  }
+                  if (state is UserCreated) {
+                    user.updateDisplayName(user.email!.split('@')[0]);
+                  }
+                  if (!user.emailVerified) {
+                    user.sendEmailVerification();
+                    const snackBar = SnackBar(
+                        content: Text(
+                            'Please check your email to verify your email address'));
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                  }
+                  context.pushReplacement('/');
+                })),
+              ],
+            );
+          },
+          //这里是sign-in的子路由
+          routes: [
+            GoRoute(
+              path: 'forgot-password',
+              builder: (context, state) {
+                final arguments = state.uri.queryParameters;
+                return ForgotPasswordScreen(
+                  email: arguments['email'],
+                  headerMaxExtent: 200,
+                );
+              },
+            ),
+          ],
+        ),
+        //这里和sign-in同级
+        GoRoute(
+          path: 'profile',
+          builder: (context, state) {
+            return ProfileScreen(
+              providers: const [],
+              actions: [
+                SignedOutAction((context) {
+                  context.pushReplacement('/');
+                }),
+              ],
+            );
+          },
+        ),
+      ],
+    ),
+  ],
+);
+
+// end of GoRouter configuration
+//App 需要依托router改造一下
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    /**** 
+    注意这里返回是.router
+    ********/
+    return MaterialApp.router(
       title: 'Firebase Meetup',
       theme: ThemeData(
         buttonTheme: Theme.of(context).buttonTheme.copyWith(
@@ -29,7 +131,11 @@ class App extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      //debugShowCheckedModeBanner: false,
+      //routeInformationProvider: _router.routeInformationProvider,
+      //routeInformationParser: _router.routeInformationParser,
+      //routerDelegate: _router.routerDelegate,
+      routerConfig: _router,
     );
   }
 }
